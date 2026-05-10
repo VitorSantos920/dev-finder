45f06cc7f221069d2f107fa40bced06c3ff14c8c
Vitor Santos <pireshugo737@gmail.com>
2026-05-09 22:58:03 -0300

    feat: adicionar hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
 import { Header } from '@/components/Header/Header';
+import { SearchInput } from './components/SearchInput/SearchInput';
+import { ArrowRight, Zap } from 'lucide-react';
 
 export function App() {
   return (
     <>
       <Header />
-      <section className="hero"></section>
       <main className="container">
-        <h1>Dev Finder</h1>
+        <section className="hero">
+          <span className="hero__powered">
+            <Zap size={14} />
+            Powered by GitHub API
+          </span>
+          <h1 className="hero__title">
+            Discover developers <br />
+            <span>through data.</span>
+          </h1>
+          <p className="hero__description">
+            Search GitHub profiles, explore repositories, analyze contribution patterns, and
+            understand any developer at a glance.
+          </p>
+
+          <form className="hero__search">
+            <SearchInput
+              iconSize={25}
+              name="hero-search"
+              id="hero-search"
+              placeholder="Search by GitHub username..."
+            />
+            <button className="hero__search-btn" type="button">
+              Search
+              <ArrowRight size={15} />
+            </button>
+          </form>
+        </section>
       </main>
     </>
   );
