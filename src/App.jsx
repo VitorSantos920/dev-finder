b5c1ba9c8b513cc26cc625a688c156c278c5091e
Vitor Santos <pireshugo737@gmail.com>
2026-05-09 23:47:46 -0300

    feat: adicionar plataform preview section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Header } from '@/components/Header/Header';
 import { SearchInput } from './components/SearchInput/SearchInput';
 import { ArrowRight, Zap } from 'lucide-react';
+import { Logo } from './components/Logo/Logo';
 
 export function App() {
   return (
@@ -34,7 +35,23 @@ export function App() {
             </button>
           </form>
         </section>
+
+        <section className="platform-preview">
+          <p className="eyebrow">PLATFORM PREVIEW</p>
+          <h2 className="platform-preview__title">
+            Everything you need to know
+            <br />
+            <span>about any developer.</span>
+          </h2>
+
+          <img src="/public/profile-mockup.webp" className="plataform-preview__exhibition" />
+        </section>
       </main>
+      <footer>
+        <Logo />
+
+        <p>Built with the GitHub API · Not affiliated with GitHub, Inc.</p>
+      </footer>
     </>
   );
 }
