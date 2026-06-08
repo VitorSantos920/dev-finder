f84ce1fe34ba7e8f17450e5790505a35f3993b28
Vitor Santos <pireshugo737@gmail.com>
2026-06-07 21:46:58 -0300

    refactor: remover forwardRef desnecessária no react 19+ | simplificação de SearchInput component | ref como common prop

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,33 +1,34 @@
 import { cn } from '@/lib/utils';
 import { Search } from 'lucide-react';
-import { forwardRef } from 'react';
+import { Ref } from 'react';
 
 type SearchInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   iconSize?: number;
+  ref?: Ref<HTMLInputElement>;
 };
 
-export const SearchInput = forwardRef<HTMLInputElement, SearchInputProps>(
-  function SearchInput(
-    { iconSize = 15, className, ...props }: SearchInputProps,
-    ref
-  ) {
-    return (
-      <>
-        <Search
-          className='group-focus-within:text-highlight duration-200 group-focus-within:rotate-90'
-          size={iconSize}
-        />
-        <input
-          className={cn(
-            'text-primary h-full w-full border-0 bg-transparent pr-2 outline-0 [&::-webkit-search-cancel-button]:hidden',
-            className
-          )}
-          ref={ref}
-          {...props}
-          type='search'
-          aria-label='Search user'
-        />
-      </>
-    );
-  }
-);
+export function SearchInput({
+  iconSize = 15,
+  className,
+  ref,
+  ...props
+}: SearchInputProps) {
+  return (
+    <>
+      <Search
+        className='group-focus-within:text-highlight duration-200 group-focus-within:rotate-90'
+        size={iconSize}
+      />
+      <input
+        className={cn(
+          'text-primary h-full w-full border-0 bg-transparent pr-2 outline-0 [&::-webkit-search-cancel-button]:hidden',
+          className
+        )}
+        ref={ref}
+        {...props}
+        type='search'
+        aria-label='Search user'
+      />
+    </>
+  );
+}
